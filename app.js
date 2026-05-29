c229d1b759c24bb6cac13bb4b7fd1ce523b9a6cc
Ubuntu <ubuntu@ip-172-31-22-62.ec2.internal>
2026-05-29 08:27:38 +0000

    adding health check

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy"
+  });
+});
